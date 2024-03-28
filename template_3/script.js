4be8e1cd3245b07f3ce97af87024d2a9fb51b7f4
bakerAllozi <160823630+bakerAllozi@users.noreply.github.com>
2024-03-28 17:05:56 +0300

    Delete template_3 directory

--- template_3/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const hd = document.querySelector('.hd') 
-const click1 = document.querySelector('.click1')
-
-click1.addEventListener('click' , function(){
-    hd.classList.toggle('hidden')
-})
